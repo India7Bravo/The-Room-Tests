8d07fefca711a4b3b846e6560f30437e44c8410d
Ismael <india7bravo@gmail.com>
2022-02-24 18:44:13 +0200

    Added the ability to update a list item

--- cypress/integration/theRoomUI.test.js
+++ cypress/integration/theRoomUI.test.js
@@ -4,17 +4,22 @@ describe('UI tests for Todo', () => {
     it('Create new todo item, toggle complete', () => {
         cy.goTo()
         //Creates multiple todo items
+        cy.get('.new-todo').click().type('This is an example of updating').type('{enter}')
         cy.get('.new-todo').click().type('This is a test of the creation of a todo').type('{enter}')
         cy.get('.new-todo').click().type('(Hopefully) Impress the reviewers').type('{enter}')
         cy.get('.new-todo').click().type('Sign offer for The Room').type('{enter}')
 
+        //Updates one of the list items
+        cy.get('li').eq(7).dblclick().clear().type('Updated' + '{enter}')
+
         //Makes sure the text is what we expected it to be as well as making sure it isn't marked as completed
-        cy.get('li').eq(7).should('have.text', 'This is a test of the creation of a todo')
+        cy.get('li').eq(8).should('have.text', 'This is a test of the creation of a todo')
         cy.get('.toggle').should('not.be.checked')
 
         //Clicking the toggle button to mark as complete and making sure the right visuals are displayed for the completed task
         cy.get('.toggle').eq(0).click()
         cy.get('.toggle').eq(1).click()
+        cy.get('.toggle').eq(2).click()
         cy.get('.view > label').should('have.css', 'text-decoration-line', 'line-through')
     })
 
